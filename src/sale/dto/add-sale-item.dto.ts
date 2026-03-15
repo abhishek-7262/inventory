d7bde1ec6612e108d938c31f9ef4596e8117eaa4
abhishek-7262 <abhishek.7262020269@gmail.com>
2026-03-15 18:52:24 +0530

    update in saleItem model

--- src/sale/dto/add-sale-item.dto.ts
+++ src/sale/dto/add-sale-item.dto.ts
@@ -1,14 +1,21 @@
-import { IsMongoId, IsNumber, Min } from 'class-validator';
+import { IsMongoId, IsNotEmpty, IsNumber, Min } from 'class-validator';
 
 export class AddSaleItemDto {
   @IsMongoId()
+  @IsNotEmpty()
   productId: string;
+
+  @IsMongoId()
+  @IsNotEmpty()
+  saleId: string;
 
   @IsNumber()
   @Min(0)
+  @IsNotEmpty()
   salePrice: number;
 
   @IsNumber()
   @Min(1)
+  @IsNotEmpty()
   saleQuantity: number;
 }
